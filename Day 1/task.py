110c384373123c749a10ea755cb4cd29df1d5000
saurabh-saw <saurabhsaw002@gmail.com>
2026-08-08 17:59:20 +0530

    Band name generator

---
+++ Day 1/task.py
@@ -0,0 +1,18 @@
+# # Print
+# print("Hello World!")
+# print ("Hello World\nHello World\nHello World")
+# print("Hello" + " " + "ToTo")
+
+# # Input
+# print("Hello " + input("What is your name? ") + "!")
+
+# # Variables
+# username = input("What is your name? ")
+# name_length = len(username)
+# print(name_length)
+
+"""
+Variable names:
+1. Should start with _, a-z, A-Z
+2. Can contain numbers
+"""
